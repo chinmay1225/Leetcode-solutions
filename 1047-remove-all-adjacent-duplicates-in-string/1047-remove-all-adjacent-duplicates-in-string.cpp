07acd71e4de68f6ed8d87cc06d07512018e63930
Chinmay Patil <patilchinmay96873@gmail.com>
2026-08-29 22:45:52 +0530

    Time: 14 ms (19.09%), Space: 14.3 MB (49.83%) - LeetHub

---
+++ 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    string removeDuplicates(string s) {
+        stack<char> st;
+
+        for(int i=0; i<s.length(); i++){
+            if(!st.empty() && st.top() == s[i]){
+                st.pop();
+            }else{
+                st.push(s[i]);
+            }
+        }
+        
+        string str = "";
+        while(!st.empty()){
+            str += st.top();
+            st.pop();
+        }
+
+        reverse(str.begin() , str.end());
+        return str;
+    }
+};
